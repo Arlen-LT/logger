9bab8e005f794149c348a8b2786553a055385f39
Arlen-LT <arlen-lt@protonmail.com>
2023-11-24 17:05:22 +0800

    using fmtlib refactor logger

--- logger.cpp
+++ logger.cpp
@@ -2,20 +2,13 @@
 
 namespace logger
 {
-#ifndef ENABLE_SLOG
-    const std::filesystem::path logFile;
     bool SetLogFile(const char *path)
     {
-        return std::filesystem::exists(const_cast<std::filesystem::path &>(logFile) = std::filesystem::u8path(path)); // TODO: change all string to u8string.
+        return std::filesystem::exists(const_cast<std::filesystem::path &>(logFile) = path);
     }
-#endif
 
     void ExternalLog(LogLevel level, const char *content)
     {
-#ifdef ENABLE_SLOG
-        slog_tag(SLOG_TAG, level, "%s\n", content);
-#else
         Log(level, "%s", content);
-#endif
     }
-} // namespace logger
+}
--- logger.h
+++ logger.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <thread>
 #include <mutex>
 #include <iostream>
 #include <ostream>
 #include <fstream>
 #include <filesystem>
 #include <regex>
+#include <functional>
+#include <concepts>
 
-#if __cpp_lib_format
-#include <format>
-#endif
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+#include <fmt/std.h>
+#include <fmt/printf.h>
 
 #if __cpp_lib_source_location
 #include <source_location>
@@ -17,8 +21,7 @@
 
 #if _WIN64
 #define gettid() std::this_thread::get_id()
-#elif __ANDROID__
-#include <android/log.h>
+#else
 #include <unistd.h>
 #endif
 
@@ -27,114 +30,71 @@
 #else
 #define LOGGER_EXPORT
 #endif
+#include "slog.h"
 
 namespace logger
 {
-#ifndef ENABLE_SLOG
-    enum class LogLevel : const char
-    {
-        Trace = 'T',
-        Debug = 'D',
-        Info = 'I',
-        Warning = 'W',
-        Error = 'E',
-        Fatal = 'F'
-    };
+     enum class LogLevel : int
+     {
+         Trace = L_TRACE,
+         Debug = L_DEBUG,
+         Info = L_INFO,
+         Warning = L_WARN,
+         Error = L_ERR,
+         Fatal = L_FATAL
+     };
 
-    extern const std::filesystem::path logFile;
+    extern "C" LOGGER_EXPORT bool SetLogFile(const char* path);
+    extern "C" LOGGER_EXPORT void ExternalLog(LogLevel level, const char* format);
 
-    extern "C" LOGGER_EXPORT bool SetLogFile(const char *path);
+    namespace {
+        const std::filesystem::path logFile;
+        bool enable_slog = true;
+    }
 
     template <typename... Args>
     struct Log
     {
-        Log(LogLevel level, const char *format, Args &&...args
+        Log(LogLevel level, const char* format, Args &&...args
 #if __cpp_lib_source_location
-            ,
-            const std::source_location &location = std::source_location::current()
+            , const std::source_location& location = std::source_location::current()
 #endif
         )
         {
-            std::ofstream logStream{logFile, std::ios::out | std::ios::app};
-            std::ostream *os = logStream.is_open() ? &logStream : &std::cout;
-            time_t current = std::time(nullptr);
-            *os << std::left
-                << std::put_time(std::localtime(&current), "%F %T ")
-                << "[" << static_cast<char>(level) << "]"
-                << "[thread " << gettid() << "] ";
-#if __cpp_lib_source_location
-            *os << location.file_name()
-                << "(" << location.line() << "," << location.column() << ","
-                << std::quoted(location.function_name())
-                << "): ";
-#endif
-#if __cpp_lib_format
-            *os << std::vformat(std::regex_replace(format, std::regex(R"(%(?:\d+\$)?[-+0 #]*\d*(?:\.\d+)?[hljztL]*[diouxXfeEgGaAcspn])"), "{}")
-                , std::make_format_args(args...)) << "\n";
-#else
-            std::string_view sv{format};
-            if constexpr (sizeof...(Args) == 0)
-                *os << sv;
-            else
+            if (enable_slog)
             {
-
-                auto TryInsert = [&sv, &os]<typename T>(size_t n, T obj)
-                {
-                    for (auto it = sv.begin(); it != sv.end(); ++it)
-                    {
-                        if (*it == '%')
-                        {
-                            if constexpr (std::is_null_pointer_v<T>)
-                                *os << "(compile-time nullptr)";
-                            else if constexpr (std::is_pointer_v<T>)
-                                obj == nullptr ? *os << "(runtime nullptr)" : *os << obj;
-                            else
-                                *os << obj;
-                            it += 2;
-                            break;
-                        }
-                        else
-                        {
-                            *os << *it;
-                        }
-                    }
-                };
-
-                [&]<std::size_t... N>(std::index_sequence<N...>)
-                {
-                    ((TryInsert(N, std::get<N>(std::forward_as_tuple(args...)))), ...);
-                }(std::index_sequence_for<Args...>{});
+                slog_tag(SLOG_TAG, static_cast<int>(level), format, args...);
+                return;
             }
 
-            *os << "\n";
-#endif // ENABLE_STD_FORMAT
+            std::string content;
+            std::time_t t = std::time(nullptr);
+            fmt::format_to(std::back_inserter(content), "{:%Y-%m-%d %H:%M:%S} [{}][{}] ", fmt::localtime(t), static_cast<int>(level), gettid());
+#if __cpp_lib_source_location
+            fmt::format_to(std::back_inserter(content), "{}({},{}): ", location.file_name(), location.line(), location.function_name());
+#endif
+#if ENABLE_OLD_C_FORMAT_COMPATIBILITY
+            content += fmt::vsprintf(std::back_inserter(content), format, fmt::make_format_args(args...));
+#else
+            fmt::vformat_to(std::back_inserter(content), format, fmt::make_format_args(args...));
+#endif
+            if (logFile.empty())
+            {
+                fmt::print("{}\n", content);
+                return;
+            }
+
+            std::ofstream logStream{ logFile, std::ios::out | std::ios::app };
+            if (logStream.is_open())
+            {
+                fmt::print(logStream, "{}\n", content);
+                logStream.close();
+            }
         }
     };
 
     template <typename... Args>
-    Log(LogLevel, const char *, Args &&...) -> Log<Args...>;
-#else // ENABLE_SLOG
-#ifndef SLOG_TAG
-#define SLOG_TAG __FILE__
-#endif
-#include "slog.h"
-    enum LogLevel
-    {
-        Trace = L_TRACE,
-        Debug = L_DEBUG,
-        Info = L_INFO,
-        Warning = L_WARN,
-        Error = L_ERR,
-        Fatal = L_FATAL
-    };
-#define Log(x, y, ...)                                \
-    do                                                \
-    {                                                 \
-        slog_tag(SLOG_TAG, x, y "\n", ##__VA_ARGS__); \
-    } while (0)
-#endif // ENABLE_SLOG
-
-    extern "C" LOGGER_EXPORT void ExternalLog(LogLevel level, const char *format);
+    Log(LogLevel, const char*, Args &&...) -> Log<Args...>;
 }
 using namespace logger;
 
